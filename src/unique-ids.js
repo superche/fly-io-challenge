994de9911ccd744f677a3976a7785403c8635673
superche <shawche@outlook.com>
2023-08-23 21:16:26 +0800

    refactor: challenge 1 snowflake

--- src/unique-ids.js
+++ src/unique-ids.js
@@ -3,13 +3,38 @@
 // A globally-unique ID generation system
 const Node = require('./core/node')
 
-const node = new Node()
-node.count = 0
+const sleepUntilNextMillisecond = () => new Promise(
+  resolve => setTimeout(resolve, 1)
+)
+const MAX_SEQUENCE = 1 << 12
+const simpleSnowflake = async node => {
+  let timestamp = Date.now()
 
-node.on('generate', req => {
-  const time = Math.floor(Date.now() / 1000)
-  const id = `${time}${node.count}${node.nodeId}`
-  node.count += 1
+  if (timestamp < node.lastTimestamp) {
+    throw new Error('Clock moved backwards')
+  } else if (timestamp === node.lastTimestamp) {
+    node.snowflakeSequence += 1
+    if (node.snowflakeSequence >= MAX_SEQUENCE) {
+      await sleepUntilNextMillisecond()
+      timestamp = Date.now()
+      node.snowflakeSequence = 0
+    }
+  } else {
+    // timestamp > lastTimestamp
+    node.snowflakeSequence = 0
+  }
+
+  node.lastTimestamp = timestamp
+
+  return `${timestamp}${node.nodeId}${node.snowflakeSequence}`
+}
+
+const node = new Node()
+node.snowflakeSequence = 0
+node.lastTimestamp = 0
+
+node.on('generate', async req => {
+  const id = await simpleSnowflake(node)
 
   node.reply(req, {
     type: 'generate_ok',
